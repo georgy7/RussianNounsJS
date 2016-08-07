160f3ae3173386a961b6acd0630e9d8253a701b7
georgy <httpcatharsis@gmail.com>
2016-08-07 12:44:29 +0300

    bugFixes

--- opencorpora-testing/singular_json.rb
+++ opencorpora-testing/singular_json.rb
@@ -68,7 +68,7 @@ class Parser < Nokogiri::XML::SAX::Document
       @word = {}
       @active_l = true
       @word[:g] = Set.new
-      @word[:cases] = [[], [], [], [], [], [], []]
+      @word[:cases] = [[], [], [], [], [], []]
       @word[:name] = get_attr(attrs, 't')
     elsif name == 'g' && @active_l && !(@word[:name].nil?)
       @word[:g].add(get_attr(attrs, 'v'))
@@ -94,7 +94,7 @@ class Parser < Nokogiri::XML::SAX::Document
     if name == 'l'
       @active_l = false
     elsif name == 'f' and @active_f
-      if !(@word_form[:name].to_s.strip.empty?) and @word_form[:g].include?('sing')
+      if !(@word_form[:name].to_s.strip.empty?) and @word_form[:g].include?('sing') and @letter_a.include?(@word_form[:name][0].mb_chars.downcase.to_s)
         if @word_form[:g].include? 'nomn'
           @word[:cases][0][0] = @word_form[:name]
         elsif @word_form[:g].include?('gen1') or (@word_form[:g].include?('gent') and !(@word_form[:g].include?('gen2')))
@@ -104,7 +104,7 @@ class Parser < Nokogiri::XML::SAX::Document
           puts "gen2: #{@word_form[:name]}"
         elsif @word_form[:g].include? 'datv'
           @word[:cases][2][0] = @word_form[:name]
-        elsif @word_form[:g].include? 'accs'
+        elsif @word_form[:g].include? 'accs' and !(@word_form[:g].include?('acc2'))
           @word[:cases][3][0] = @word_form[:name]
         elsif @word_form[:g].include? 'acc2'
           @word[:cases][3][1] = @word_form[:name] if @word[:cases][3].size <= 1
@@ -116,8 +116,11 @@ class Parser < Nokogiri::XML::SAX::Document
           @word[:cases][5][0] = @word_form[:name]
         elsif @word_form[:g].include? 'loc2'
           @word[:cases][5][1] = @word_form[:name]
+          puts "loc2: #{@word_form[:name]}"
         elsif @word_form[:g].include? 'voct'
+          @word[:cases][6] = []
           @word[:cases][6][0] = @word_form[:name] # Vocative case
+          puts "voct: #{@word_form[:name]}"
         end
       end
       @active_f = false
@@ -140,6 +143,16 @@ class Parser < Nokogiri::XML::SAX::Document
             :g => @word[:g],
             :cases => @word[:cases]
           })
+          
+          # Warnings
+          @word[:cases].each_with_index { |c, index|
+            if [1,3,5].include?(index) and (c.size > 1) and (c.uniq.size < c.size)
+              case_name = 'genitive case' if index == 1
+              case_name = 'accusative case' if index == 3
+              case_name = 'locative case' if index == 5
+              puts "Dublicates of the #{case_name} of \"#{@word[:name]}\": #{@word[:cases][index]}"
+            end
+          }
         end
 
       end
@@ -169,7 +182,24 @@ abc = [
 ]
 
 ram_file = IO.read(filename)
-Parallel.each(abc, :in_processes => 4) { |letter|
+part = abc
+
+# On Windows just run from different terminals:
+# ruby singular_json.rb 1 > singular_json_output_1.txt
+# ruby singular_json.rb 2 > singular_json_output_2.txt
+# ruby singular_json.rb 3 > singular_json_output_3.txt
+# ruby singular_json.rb 4 > singular_json_output_4.txt
+if !(ARGV.empty?)
+  STDERR.puts "ARGV = #{ARGV}"
+  part = abc[0..8] if (ARGV[0].to_s == '1')
+  part = abc[9..16] if (ARGV[0].to_s == '2')
+  part = abc[17..24] if (ARGV[0].to_s == '3')
+  part = abc[25..(abc.size - 1)] if (ARGV[0].to_s == '4')
+  puts "Part #{ARGV[0]}"
+  STDERR.puts "Part #{ARGV[0]}"
+end
+
+Parallel.each(part, :in_processes => 4) { |letter|
   p = Parser.new letter
   fn = output_filename_template.sub('LETTER', letter[0])
   puts "Started #{fn}..."
