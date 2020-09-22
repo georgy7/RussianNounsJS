e5950f07cd293bcf972a7b1b6eb64cb609cc4a2b
Georgy Ustinov <httpcatharsis@gmail.com>
2020-09-22 05:26:33 +0300

    Refactoring tests. NgTable.

--- js/testRunner.js
+++ js/testRunner.js
@@ -1,192 +1,219 @@
-﻿(function() {
-	angular.module('testing', []);
+﻿(function () {
 
-	angular.module('testing').controller('TestRunnerController', TestRunnerController);
+    let app = angular.module('testApp', [
+        'ngTable'
+    ]);
 
-	TestRunnerController.$inject = [
-		'$scope'
-	];
+    //--------------------------------------------------------
 
-	function TestRunnerController(
-		$scope
-	) {
+    app.controller('TestRunnerController', TestRunnerController);
 
-		const abc = "абвгдежзийклмнопрстуфхцчшщъыьэюя".split('');
-		const parts = [];
-		parts.push(abc.slice(0, 5));
-		parts.push(abc.slice(5, 12));
-		parts.push(abc.slice(12, 18));
-		parts.push(abc.slice(18, abc.length));
+    TestRunnerController.$inject = [
+        '$scope',
+        'NgTableParams'
+    ];
 
-		$scope.loadingStatuses = [
-			_(parts[0].length).times(function(){ return null; }),
-			_(parts[1].length).times(function(){ return null; }),
-			_(parts[2].length).times(function(){ return null; }),
-			_(parts[3].length).times(function(){ return null; })
-		];
+    function TestRunnerController(
+        $scope,
+        NgTableParams
+    ) {
 
-		$scope.results = [[], [], [], []];
-		$scope.completed = [false, false, false, false];
+        const abc = "абвгдежзийклмнопрстуфхцчшщъыьэюя".split('');
+        const parts = [];
+        parts.push(abc.slice(0, 5));
+        parts.push(abc.slice(5, 12));
+        parts.push(abc.slice(12, 18));
+        parts.push(abc.slice(18, abc.length));
 
-		$scope.workers = [];
-		$scope.workers.push(new Worker('js/test.js'));
-		$scope.workers.push(new Worker('js/test.js'));
-		$scope.workers.push(new Worker('js/test.js'));
-		$scope.workers.push(new Worker('js/test.js'));
+        $scope.loadingStatuses = [
+            _(parts[0].length).times(function () {
+                return null;
+            }),
+            _(parts[1].length).times(function () {
+                return null;
+            }),
+            _(parts[2].length).times(function () {
+                return null;
+            }),
+            _(parts[3].length).times(function () {
+                return null;
+            })
+        ];
 
-		$scope.runLetter = (workerIndex, letterIndex) => {
-			var worker = $scope.workers[workerIndex];
-			var letter = parts[workerIndex][letterIndex];
-			if (!letter) {
-				throw 'Out of bound of letter list index.';
-			}
-			jQuery.get('opencorpora-testing/nouns_singular_' + letter + '.json', function (words) {
-				worker.postMessage({
-					type: 'start',
-					words: words,
-					workerIndex: workerIndex,
-					letterIndex: letterIndex
-				});
-			});
-		};
+        $scope.results = [[], [], [], []];
+        $scope.completed = [false, false, false, false];
 
-		$scope.listenEvents = workerIndex => {
-			$scope.workers[workerIndex].onmessage = function(e) {
-				if (e.data.type === 'loading') {
-					$scope.loadingStatuses[e.data.workerIndex][e.data.letterIndex] = e.data;
-					$scope.updateLoading($scope.calculateLoading());
-				} else if (e.data.type === 'testResult') {
-					console.log('' + parts[e.data.workerIndex][e.data.letterIndex] + ' completed');
-					$scope.results[e.data.workerIndex][e.data.letterIndex] = e.data;
-					var next = e.data.letterIndex + 1;
-					if (parts[e.data.workerIndex].length > next) {
-						$scope.runLetter(e.data.workerIndex, next);
-					} else {
-						console.log(new Date(), 'Process ' + (1 + e.data.workerIndex) + ' completed');
-						$scope.completed[e.data.workerIndex] = true;
-						if ($scope.completed.every(x => x)) {
-							$scope.$apply(() => {
-								$scope.showResults();
-							});
-						}
-					}
-				}
-			};
-		};
+        $scope.workers = [];
+        $scope.workers.push(new Worker('js/test.js'));
+        $scope.workers.push(new Worker('js/test.js'));
+        $scope.workers.push(new Worker('js/test.js'));
+        $scope.workers.push(new Worker('js/test.js'));
 
-		$scope.showResults = () => {
-			console.log(new Date(), 'Finish.');
-			let totalCases = 0;
-			let wrongCases = 0;
-			let totalWords = 0;
-			let correctWordsWithWarnings = 0;
-			let wrongWords = 0;
-			let items = [];
+        $scope.runLetter = (workerIndex, letterIndex) => {
+            var worker = $scope.workers[workerIndex];
+            var letter = parts[workerIndex][letterIndex];
+            if (!letter) {
+                throw 'Out of bound of letter list index.';
+            }
+            jQuery.get('opencorpora-testing/nouns_singular_' + letter + '.json', function (words) {
+                worker.postMessage({
+                    type: 'start',
+                    words: words,
+                    workerIndex: workerIndex,
+                    letterIndex: letterIndex
+                });
+            });
+        };
 
-			let itemLen = 0;
+        $scope.listenEvents = workerIndex => {
+            $scope.workers[workerIndex].onmessage = function (e) {
+                if (e.data.type === 'loading') {
+                    $scope.loadingStatuses[e.data.workerIndex][e.data.letterIndex] = e.data;
+                    $scope.updateLoading($scope.calculateLoading());
+                } else if (e.data.type === 'testResult') {
+                    console.log('' + parts[e.data.workerIndex][e.data.letterIndex] + ' completed');
+                    $scope.results[e.data.workerIndex][e.data.letterIndex] = e.data;
+                    var next = e.data.letterIndex + 1;
+                    if (parts[e.data.workerIndex].length > next) {
+                        $scope.runLetter(e.data.workerIndex, next);
+                    } else {
+                        console.log(new Date(), 'Process ' + (1 + e.data.workerIndex) + ' completed');
+                        $scope.completed[e.data.workerIndex] = true;
+                        if ($scope.completed.every(x => x)) {
+                            $scope.$apply(() => {
+                                $scope.showResults();
+                            });
+                        }
+                    }
+                }
+            };
+        };
 
-			for (let eArray of $scope.results) {
-				for (let data of eArray) {
-					totalCases += data.totalCases;
-					wrongCases += data.wrongCases;
-					totalWords += data.totalWords;
-					correctWordsWithWarnings += data.correctWordsWithWarnings;
-					wrongWords += data.wrongWords;
-					items = items.concat(data.resultForTemplate.items);
-					itemLen += data.resultForTemplate.items.length;
-				}
-			}
+        $scope.showResults = () => {
+            console.log(new Date(), 'Finish.');
+            let totalCases = 0;
+            let wrongCases = 0;
+            let totalWords = 0;
+            let correctWordsWithWarnings = 0;
+            let wrongWords = 0;
+            let items = [];
 
-			console.log('---------------');
-			console.log(itemLen);
-			console.log(items.length);
-			console.log('---------------');
-			console.log(items);
+            let itemLen = 0;
 
-			$scope.items = items;
-			$scope.itemsViewPosition = 0;
-			$scope.updateItemsView();
+            for (let eArray of $scope.results) {
+                for (let data of eArray) {
+                    totalCases += data.totalCases;
+                    wrongCases += data.wrongCases;
+                    totalWords += data.totalWords;
+                    correctWordsWithWarnings += data.correctWordsWithWarnings;
+                    wrongWords += data.wrongWords;
+                    items = items.concat(data.resultForTemplate.items);
+                    itemLen += data.resultForTemplate.items.length;
+                }
+            }
 
-			$scope.wordsCorrect = totalWords - wrongWords;
-			$scope.wordsTotal = totalWords;
-			$scope.wordsCorrectShare = $scope.wordsCorrect / $scope.wordsTotal * 100;
+            console.log('---------------');
+            console.log(itemLen);
+            console.log(items.length);
+            console.log('---------------');
+            console.log(items);
 
-			$scope.wordFormsCorrect = totalCases - wrongCases;
-			$scope.wordFormsTotal = totalCases;
-			$scope.wordFormsCorrectShare = $scope.wordFormsCorrect / $scope.wordFormsTotal * 100;
+            $scope.items = items;
+            $scope.wordTableParams.reload();
 
-			$scope.wordsHasWarnings = correctWordsWithWarnings;
-			$scope.wordsHasWarningsShare = correctWordsWithWarnings / totalWords * 100;
-		};
+            $scope.wordsCorrect = totalWords - wrongWords;
+            $scope.wordsTotal = totalWords;
+            $scope.wordsCorrectShare = $scope.wordsCorrect / $scope.wordsTotal * 100;
 
-		$scope.itemsViewPageSize = 10;
+            $scope.wordFormsCorrect = totalCases - wrongCases;
+            $scope.wordFormsTotal = totalCases;
+            $scope.wordFormsCorrectShare = $scope.wordFormsCorrect / $scope.wordFormsTotal * 100;
 
-		$scope.updateItemsView = () => {
-			if (($scope.itemsViewPosition == null) || !($scope.items instanceof Array)) {
-				return;
-			}
-			$scope.itemsView = $scope.items.slice(
-				$scope.itemsViewPosition,
-				$scope.itemsViewPosition + $scope.itemsViewPageSize
-			);
-		};
+            $scope.wordsHasWarnings = correctWordsWithWarnings;
+            $scope.wordsHasWarningsShare = correctWordsWithWarnings / totalWords * 100;
+        };
 
-		$scope.calculateLoading = () => {
-			let count = 0;
-			let sum = 0;
-			for (var i = 0; i < $scope.loadingStatuses.length; i++) {
-				var arr = $scope.loadingStatuses[i];
-				for (var j = 0; j < arr.length; j++) {
-					if (arr[j]) {
-						sum += arr[j].status;
-					}
-					count++;
-				}
-			}
-			return sum/count;
-		};
+        $scope.filterWords = () => {
+            if ($scope.items && $scope.items.length) {
+                // TODO: filter
+                return $scope.items;
+            } else {
+                return [];
+            }
+        };
 
-		$scope.updateLoading = (loadStatus) => {
-			let barWidth = '' + Math.round(100 * loadStatus) + '%';
-			jQuery('#loadingBar .status').css('width', barWidth);
-		};
+        $scope.wordTableParams = new NgTableParams({
+            page: 1,
+            count: 10
+        }, {
+            total: 0,
+            counts: [10, 25, 100, 1000],
+            getData: (params) => {
+                const filtered = $scope.filterWords();
+                params.total(filtered.length);
 
-		console.log(new Date(), 'Start.');
-		for (let w = 0; w < $scope.workers.length; w++) {
-			$scope.listenEvents(w);
-			$scope.runLetter(w, 0);
-		}
+                const start = (params.page() - 1) * params.count();
+                const stop = start + params.count();
 
-		$scope.genderColor = (item) => {
-			const g = item.gender.toLowerCase();
-			if (g.startsWith('м')) { return "#df5"; }
-			if (g.startsWith('ж')) { return "#9f5"; }
-			if (g.startsWith('с')) { return "#f59"; }
-			return "#000";
-		};
+                return filtered.slice(start, stop);
+            }
+        });
 
-		$scope.declensionColor = (item) => {
-			const d = item.declension;
-			if (d === '') { return '#999999'; }
-			if (d === 1) { return '#3ef481'; }
-			if (d === 2) { return '#96f43e'; }
-			if (d === 3) { return '#f3f43e'; }
-			return '#fff';
-		};
+        $scope.calculateLoading = () => {
+            let count = 0;
+            let sum = 0;
+            for (var i = 0; i < $scope.loadingStatuses.length; i++) {
+                var arr = $scope.loadingStatuses[i];
+                for (var j = 0; j < arr.length; j++) {
+                    if (arr[j]) {
+                        sum += arr[j].status;
+                    }
+                    count++;
+                }
+            }
+            return sum / count;
+        };
 
-		$scope.toPreviousPage = () => {
-			let newValue = $scope.itemsViewPosition - $scope.itemsViewPageSize;
-			$scope.itemsViewPosition = (newValue < 0) ? 0 : newValue;
-			$scope.updateItemsView();
-		};
+        $scope.updateLoading = (loadStatus) => {
+            let barWidth = '' + Math.round(100 * loadStatus) + '%';
+            jQuery('#loadingBar .status').css('width', barWidth);
+        };
 
-		$scope.toNextPage = () => {
-			let lastPossibleValueOnThePageNow = $scope.itemsViewPosition + $scope.itemsViewPageSize - 1;
-			if (lastPossibleValueOnThePageNow >= ($scope.items.length - 1)) {
-				return;
-			}
-			$scope.itemsViewPosition = $scope.itemsViewPosition + $scope.itemsViewPageSize;
-			$scope.updateItemsView();
-		};
-	}
+        console.log(new Date(), 'Start.');
+        for (let w = 0; w < $scope.workers.length; w++) {
+            $scope.listenEvents(w);
+            $scope.runLetter(w, 0);
+        }
+
+        $scope.genderColor = (item) => {
+            const g = item.gender.toLowerCase();
+            if (g.startsWith('м')) {
+                return "#df5";
+            }
+            if (g.startsWith('ж')) {
+                return "#9f5";
+            }
+            if (g.startsWith('с')) {
+                return "#f59";
+            }
+            return "#000";
+        };
+
+        $scope.declensionColor = (item) => {
+            const d = item.declension;
+            if (d === '') {
+                return '#999999';
+            }
+            if (d === 1) {
+                return '#3ef481';
+            }
+            if (d === 2) {
+                return '#96f43e';
+            }
+            if (d === 3) {
+                return '#f3f43e';
+            }
+            return 'transparent';
+        };
+    }
 })();
