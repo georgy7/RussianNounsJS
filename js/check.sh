d6115a129022e1f393db537f550790f7a1883468
Georgy Ustinov <inbox@itustinov.ru>
2025-06-17 17:23:20 +0300

    Fixed minification

--- js/check.sh
+++ js/check.sh
@@ -17,7 +17,7 @@ npx es-check es6 RussianNouns.js --module
 ls -l RussianNouns.js
 
 uglifyjs \
-        --mangle \
+        --mangle --module \
         --compress passes=2 \
         --beautify indent_level=4 \
         --comments "/@license|@preserve|^!/" \
@@ -25,10 +25,20 @@ uglifyjs \
 
 ls -l RussianNouns.min.js
 
+# Replacing spaces with tabs (for compactness)...
 unexpand -t 4 RussianNouns.min.js > RussianNouns.min.temp.js
+
+# Removing spaces from the switch-case...
 sed -i 's/\t  case /\tcase /' RussianNouns.min.temp.js
 sed -i 's/\t  default/\tdefault/' RussianNouns.min.temp.js
+
+# Removing trailing spaces...
 sed -i 's/\s\+$//' RussianNouns.min.temp.js
+
+# Fixing "No newline at end of file"
+# https://unix.stackexchange.com/a/31955
+sed -i -e '$a\' RussianNouns.min.temp.js
+
 mv RussianNouns.min.temp.js RussianNouns.min.js
 
 ls -l RussianNouns.min.js
