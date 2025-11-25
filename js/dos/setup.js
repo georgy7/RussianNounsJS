aaa773d8ba0a932161ccfb99a079997ae723bacf
Georgy Ustinov <inbox@itustinov.ru>
2025-11-26 01:36:18 +0300

    jSH CP866: MVP

--- js/dos/setup.js
+++ js/dos/setup.js
@@ -13,21 +13,58 @@
 // https://opensource.org/license/0bsd
 
 
-function cp866(u) {
-    var result = 0x04;
+"use strict";
 
-    if (0x20 <= u && u <=0x7E) {
-        result = u;
-    } else if (0x410 <= u && u <= 0x43F) {
-        result = u - 0x390;
-    } else if (0x440 <= u && u <= 0x44F) {
-        result = u - 0x360;
+
+var cp866 = (function() {
+    var codes = {};
+
+    codes[0] = 0;
+    codes[0x2302] = 0x7F;   // ⌂
+
+    var top = '' +
+        '☺☻♥♦♣♠•◘○◙♂♀♪♫☼' +
+        '►◄↕‼¶§▬↨↑↓→←∟↔▲▼';
+
+    var frames = '' +
+        '░▒▓│┤╡╢╖╕╣║╗╝╜╛┐' +
+        '└┴┬├─┼╞╟╚╔╩╦╠═╬╧' +
+        '╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀';
+
+    var fx = 'ЁёЄєЇїЎў°∙·√№¤■\xA0';
+    codes[0x0406] = 0x49;   // І (Cyrillic)
+    codes[0x0456] = 0x69;   // і (Cyrillic)
+
+    var i;
+
+    for (i = 0; i < top.length; i++) {
+        codes[top.charCodeAt(i)] = 0x01 + i;
     }
 
-    // TODO
+    for (i = 0; i < frames.length; i++) {
+        codes[frames.charCodeAt(i)] = 0xB0 + i;
+    }
 
-    return AsciiCharDef(result);
-}
+    for (i = 0; i < fx.length; i++) {
+        codes[fx.charCodeAt(i)] = 0xF0 + i;
+    }
+
+    return function (u) {
+        var result = 0x04;
+
+        if (0x20 <= u && u <=0x7E) {
+            result = u;
+        } else if (0x410 <= u && u <= 0x43F) {
+            result = u - 0x390;
+        } else if (0x440 <= u && u <= 0x44F) {
+            result = u - 0x360;
+        } else if (codes.hasOwnProperty(u)) {
+            result = codes[u];
+        }
+
+        return AsciiCharDef(result);
+    };
+})();
 
 function puts(str) {
     for (var i = 0; i < str.length; i++) {
@@ -75,7 +112,7 @@ function main() {
     Println();
 
     Print("Look: ");
-    puts("Привет!");
+    puts("привет");
     for (var i = 0; i < 5; i++) {
         switch (yesNo("Is that readable? (Y/N) ")) {
             case true:
