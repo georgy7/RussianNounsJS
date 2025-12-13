081976b875cc3eb0720ba3615ec174074691a39c
Georgy Ustinov <inbox@itustinov.ru>
2025-12-13 09:34:33 +0300

    jSH Screen(): it almost works

--- js/dos/simple.js
+++ js/dos/simple.js
@@ -1,19 +1,26 @@
 "use strict";
 
-function printLine(startCode) {
-    Print((0 == startCode ? '0' : '') + startCode.toString(16).toUpperCase());
-    PutCh(AsciiCharDef(0x20));
-    PutCh(AsciiCharDef(0x20));
-    for (var i = 0; i < 16; i++) {
-        PutCh(AsciiCharDef(startCode + i));
-        PutCh(AsciiCharDef(0x20));
-    }
-    Print('\r\n');
-}
-
 Println();
 
-for (var i = 0; i < 16; i++) {
-    printLine(0x10 * i);
+for (var i = 0; i <= 0xF; i++) {
+    Println(i.toString(16).toUpperCase() + '0');
 }
+
+var y = WhereY();
+var screen = new Screen();
+
+screen.FromDisplay();
+
+for (var i = 0; i <= 0xF; i++) {
+    var lineNumber = y - 0x10 + i;
+    if (lineNumber >= 1) {
+        for (var j = 0; j <= 0xF; j++) {
+            screen.Put(5 + 2 * j, lineNumber, AsciiCharDef(0x10 * i + j));
+        }
+    }
+}
+
+screen.ToDisplay();
+
+Println();
 
