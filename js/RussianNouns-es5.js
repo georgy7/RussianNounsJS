eb28d08f5dddd7db11ab855a4c6a01fa817b2887
Georgy Ustinov <inbox@itustinov.ru>
2026-01-06 08:28:43 +0300

    Another minor change in check.sh

--- js/RussianNouns-es5.js
+++ js/RussianNouns-es5.js
@@ -13,6 +13,7 @@ function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), 
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+
 /*!
   RussianNounsJS v2.5.0
   Copyright (c) 2011-2026 Georgy Ustinov
