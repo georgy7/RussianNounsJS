ded27aaa48f3612db0e306fe75b383e631eb2ce8
Georgy Ustinov <inbox@itustinov.ru>
2026-02-20 18:03:50 +0300

    Refactored

--- src/settings/defaultStressDictionary.js
+++ src/settings/defaultStressDictionary.js
@@ -2,103 +2,66 @@ import { StressDictionary } from "../StressDictionary.js";
 import { Gender } from "../Gender.js";
 
 export function makeDefaultStressDictionary() {
-    function putAll(dictionary, lemmaPrototype, value, joinedWordList) {
+    let lemmaOptions;
+    const dictionary = new StressDictionary();
+
+    function putAll(value, joinedWordList) {
         const list = joinedWordList.split(',');
         for (let word of list) {
-            const lemma = Object.assign({}, lemmaPrototype);
-            lemma.text = word;
-            dictionary.put(lemma, value);
+            lemmaOptions.text = word;
+            dictionary.put(lemmaOptions, value);
         }
     }
 
-    const d = new StressDictionary();
-    const m = Object.freeze({gender: Gender.MASCULINE});
-    const ma = Object.freeze({gender: Gender.MASCULINE, animate: true});
-    const f = Object.freeze({gender: Gender.FEMININE});
-    const fa = Object.freeze({gender: Gender.FEMININE, animate: true});
-    const ca = Object.freeze({gender: Gender.COMMON, animate: true});
-    const putM = (settings, word) => putAll(d, m, settings, word);
+    lemmaOptions = {pluraleTantum: true};
+    putAll('SSSSSSS-SSSSSS', 'ножны');
 
-    putAll(d, m,
-        'SSSSSSS-SSSSSS',
-        'брёх,дёрн,идиш,имидж,мед,упрёк');
-
-    putAll(d, {pluraleTantum: true},
-        'SSSSSSS-SSSSSS',
-        'ножны');
-
-    putAll(d, m,
-        'SSSSSSS-EEEEEE',
-        'адрес,век,вечер,город,детдом,поезд,спецсчёт,субсчёт');
-
-    putAll(d, m,
-        'SSSSSSE-EEEEEE',
-        'берег,бок,вес,лес,снег,дом,катер,счёт,мёд');
-
-    putAll(d, ma,
-        'SSSSSSS-SSSSSS',
-        'балансёр,шофёр');
-
-    putAll(d, m,
-        'SSSSSSS-bbbbbb',
-        'вексель,ветер');
-
-    putM('SSSSSSE-ESEEEE', 'глаз');
-    putM('SSSSSSE-bEEbEE', 'год');
-    putM('SSSSSSb-bbbbbb', 'цех');
-
-    putAll(d, {gender: Gender.NEUTER},
-        'EEEEEEE-SSSSSS',
-        'тесло,' +
-        'стекло,автостекло,бронестекло,оргстекло,' +
-        'пеностекло,смарт-стекло,спецстекло,' +
-        'бедро,берцо,блесна,чело,стегно,стебло');
-
-    putAll(d, f, 'EEEbEEE-SSESEE', 'щека');
-    putAll(d, f, 'EEEEEEE-SSESEE', 'слеза');
+    lemmaOptions = {gender: Gender.MASCULINE};
+    putAll('SSSSSSS-SSSSSS', 'брёх,дёрн,идиш,имидж,мед,упрёк');
+    putAll('SSSSSSS-EEEEEE', 'адрес,век,вечер,город,детдом,поезд,спецсчёт,субсчёт');
+    putAll('SSSSSSE-EEEEEE', 'берег,бок,вес,лес,снег,дом,катер,счёт,мёд');
+    putAll('SSSSSSS-bbbbbb', 'вексель,ветер');
+    putAll('SSSSSSE-ESEEEE', 'глаз');
+    putAll('SSSSSSE-bEEbEE', 'год');
+    putAll('SSSSSSb-bbbbbb', 'цех');
 
     // Почти все слова на ж/ш/ч/ц с ударением на окончание
     // захешированы (см. stressHashes).
+    putAll('SbbSbbb-bbbbbb', 'грош,шприц');
+    putAll('SssSsss-ssssss', 'кишмиш,кряж,слеш,слэш');
+    putAll('SEESeEE-EEEEEE', 'стеллаж');
+    putAll('SeeSeee-eeeeee', 'шиномонтаж');
 
-    putAll(d, m,
-        'SbbSbbb-bbbbbb',
-        'грош,шприц');
+    lemmaOptions = {gender: Gender.MASCULINE, animate: true};
+    putAll('Sssssss-ssssss', 'паныч');
+    putAll('SSSSSSS-SSSSSS', 'балансёр,шофёр');
 
-    putAll(d, m,
-        'SssSsss-ssssss',
-        'кишмиш,' +
-        'кряж,' +  // обрубок бревна; гряда холмов
-        'слеш,слэш');
+    lemmaOptions = {gender: Gender.NEUTER};
+    putAll('EEEEEEE-SsESEE', 'плечо');
+    putAll('EEEEEEE-SSSSSS',
+            'тесло,' +
+            'стекло,автостекло,бронестекло,оргстекло,' +
+            'пеностекло,смарт-стекло,спецстекло,' +
+            'бедро,берцо,блесна,чело,стегно,стебло');
 
-    putAll(d, ma,
-        'Sssssss-ssssss',
-        'паныч');
-
-    putM('SEESeEE-EEEEEE', 'стеллаж');
-    putM('SeeSeee-eeeeee', 'шиномонтаж');
-
-    putAll(d, {gender: Gender.NEUTER},
-        'EEEEEEE-SsESEE',
-        'плечо');
-
+    lemmaOptions = {gender: Gender.FEMININE};
+    putAll('EEEbEEE-SSESEE', 'щека');
+    putAll('EEEEEEE-SSESEE', 'слеза');
     // Если основа слова заканчивается на буквы жшчщц,
     // от ударения зависит окончание творительного падежа ед.ч.
     // В остальных словах ударение влияет на окончание в р.п. мн.ч.
+    putAll('EEEEEEE-SESSSS', 'семья,макросемья');
+    putAll('EEEEEEE-SEESEE', 'вожжа,свеча');
+    putAll('EEESEEE-SSSSSS', 'душа');
+    putAll('EEEEEEE-eEeeee', 'скамья');
+    putAll('EEEEEEE-EEEEEE', 'башка,кишка,ладья,лапша,моча,пыльца,статья');
 
-    putAll(d, ca, 'EEEEEEE-SSSSSS', 'судья');
-    putAll(d, ca, 'EEEEEEE-EEEEEE', 'левша');
+    lemmaOptions = {gender: Gender.FEMININE, animate: true};
+    putAll('EEEEEEE-SESESS', 'свинья,овца');
 
-    putAll(d, f, 'EEEEEEE-SESSSS', 'семья,макросемья');
-    putAll(d, f, 'EEEEEEE-SEESEE', 'вожжа,свеча');
-    putAll(d, f, 'EEESEEE-SSSSSS', 'душа');
+    lemmaOptions = {gender: Gender.COMMON, animate: true};
+    putAll('EEEEEEE-SSSSSS', 'судья');
+    putAll('EEEEEEE-EEEEEE', 'левша');
 
-    putAll(d, fa, 'EEEEEEE-SESESS', 'свинья,овца');
-
-    putAll(d, f, 'EEEEEEE-eEeeee', 'скамья');
-
-    putAll(d, f,
-        'EEEEEEE-EEEEEE',
-        'башка,кишка,ладья,лапша,моча,пыльца,статья');
-
-    return d;
+    return dictionary;
 }
--- src/settings/locativeDictionary.js
+++ src/settings/locativeDictionary.js
@@ -5,10 +5,10 @@ import { Lemma } from "../Lemma.js";
 function makeDefaultLocativeDictionary() {
     const map = new Map();
 
-    const m = Object.freeze({gender: Gender.MASCULINE});
-    const mAnimate = Object.freeze({gender: Gender.MASCULINE, animate: true});
+    const masc = {gender: Gender.MASCULINE};
+    const mascAnimate = {gender: Gender.MASCULINE, animate: true};
 
-    function addConfig(lemmaPrototype, condition, prepositions, ws, dTypes) {
+    function addConfig(lemmaOptions, condition, prepositions, ws, dTypes) {
         const words = ws.split(',');
 
         // Тут если номер, то это LocativeDeclensionType,
@@ -18,10 +18,9 @@ function makeDefaultLocativeDictionary() {
         const declensionTypes = (dTypes instanceof Array) ? dTypes : [LocativeDeclensionType.U_SUFFIX];
 
         for (let word of words) {
-            const lemma = Object.assign({}, lemmaPrototype);
-            lemma.text = word;
+            lemmaOptions.text = word;
 
-            const lemmaKey = toLocativeDictionaryKey(Lemma.create(lemma));
+            const lemmaKey = toLocativeDictionaryKey(Lemma.create(lemmaOptions));
 
             let configArray = map.get(lemmaKey);
             if (!configArray) {
@@ -40,38 +39,38 @@ function makeDefaultLocativeDictionary() {
     // В. А. Плунгян выделяет у слов мужского рода
     // с особыми формами локатива семь семантических классов:
 
-    const v = Object.freeze([LocativePreposition.V]);
-    const vo = Object.freeze([LocativePreposition.VO]);
-    const na = Object.freeze([LocativePreposition.NA]);
+    const v = [LocativePreposition.V];
+    const vo = [LocativePreposition.VO];
+    const na = [LocativePreposition.NA];
 
     // 1. вместилища, сосуды («в»)
-    addConfig(m, LocativeFormAttribute.CONTAINER, v, 'мозг,пруд,стог,таз,год');
-    addConfig(m, LocativeFormAttribute.CONTAINER, vo, 'рот');
+    addConfig(masc, LocativeFormAttribute.CONTAINER, v, 'мозг,пруд,стог,таз,год');
+    addConfig(masc, LocativeFormAttribute.CONTAINER, vo, 'рот');
     // Год может быть тем, в чём содержатся дни, например,
     // и может быть тем, на чём лежат события.
     // Это два разных случая. Их нельзя в один конфиг помещать,
     // т.к. у них условия через конъюнкцию проверяются.
-    addConfig(m, LocativeFormAttribute.WAY, v, 'год');
-    addConfig(m, LocativeFormAttribute.CONTAINER, v, 'гроб');
+    addConfig(masc, LocativeFormAttribute.WAY, v, 'год');
+    addConfig(masc, LocativeFormAttribute.CONTAINER, v, 'гроб');
     // Не уверен, что семантика "во гробе" тут правильная.
     // Не исключено, что это имеет совершенно другой религиозный смысл, чем вместилище,
     // поэтому и склонение отличается.
-    addConfig(m, LocativeFormAttribute.CONTAINER|LocativeFormAttribute.RELIGIOUS,
+    addConfig(masc, LocativeFormAttribute.CONTAINER|LocativeFormAttribute.RELIGIOUS,
         vo, 'гроб', [LocativeDeclensionType.PREPOSITIONAL]);
 
     // 2. пространства («в»)
-    addConfig(m, LocativeFormAttribute.LOCATION, v,
+    addConfig(masc, LocativeFormAttribute.LOCATION, v,
         'ад,бор,лес,порт,аэропорт,рай,сад,детсад,тыл,' +
         'низ,' +
         'хлев'  // по классификации Плунгяна, это вместилище (как и "цех")
     );
 
     // 3. конфигурации объектов, образующих устойчивые структуры («в»)
-    addConfig(m, LocativeFormAttribute.STRUCTURE, v,
+    addConfig(masc, LocativeFormAttribute.STRUCTURE, v,
         'круг,полк,артполк,ряд,род,строй,лад');
 
     // 4. поверхности («на»)
-    addConfig(m, LocativeFormAttribute.SURFACE, na, '' +
+    addConfig(masc, LocativeFormAttribute.SURFACE, na, '' +
         'баз,' +    // скотный двор
         'берег,' +
         'бережок,' +    // (спорно)
@@ -79,71 +78,71 @@ function makeDefaultLocativeDictionary() {
     );
     // На своём веку, столько-то раз на дню.
     // При этом, в веке — 100 лет, в дне — 24 часа.
-    addConfig(m, LocativeFormAttribute.WAY, na, 'век,день');
+    addConfig(masc, LocativeFormAttribute.WAY, na, 'век,день');
     // Это читерство небольшое, но тут аналогичная ситуация.
-    addConfig(m, LocativeFormAttribute.WAY, v, 'час');
+    addConfig(masc, LocativeFormAttribute.WAY, v, 'час');
     // "на корню" — устойчивое выражение (наречие), означающее "в процессе формирования".
     // "зарубить на корню" — "уничтожить в самом начале".
-    addConfig(m, LocativeFormAttribute.WAY, na, 'корень');
+    addConfig(masc, LocativeFormAttribute.WAY, na, 'корень');
 
     // 5. объекты с функциональной (не обязательно плоской) поверхностью («на»)
-    addConfig(mAnimate, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, 'вор');
-    addConfig(m, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, '' +
+    addConfig(mascAnimate, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, 'вор');
+    addConfig(masc, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, '' +
         'повод,' +  // ремень упряжки (возможно, обстоятельство - метафорическая упряжка, не уверен)
         'бочок,' +  // лежать на бочку, т.е. лежать боком вниз (почти не употребляется)
         'борт,воз,горб,кол,мост,плот,сук,' +
         'х' + String.fromCharCode(1091) + 'й'
     );
-    addConfig(m, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, '' +
+    addConfig(masc, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, '' +
         'крюк,болт',
         [LocativeDeclensionType.PREPOSITIONAL, LocativeDeclensionType.U_SUFFIX]
     );
 
     // 6. вещества и материалы («в» и «на»)
     const substance_or_resource = ',мёд,мех,пар,пух';
-    addConfig(m, LocativeFormAttribute.SUBSTANCE, v, 'дым,жир,мел,пушок' + substance_or_resource);
-    addConfig(m, LocativeFormAttribute.RESOURCE, na, 'газ,клей,спирт' + substance_or_resource);
+    addConfig(masc, LocativeFormAttribute.SUBSTANCE, v, 'дым,жир,мел,пушок' + substance_or_resource);
+    addConfig(masc, LocativeFormAttribute.RESOURCE, na, 'газ,клей,спирт' + substance_or_resource);
 
     // 7. ситуации и состояния («в» и «на»)
-    addConfig(m, LocativeFormAttribute.CONDITION, v,
+    addConfig(masc, LocativeFormAttribute.CONDITION, v,
         'бой,бред,быт,долг,плен,пыл,сок,ход,лад');
     // Тут я имею в виду смысл, употреблённый в текущем предложении.
     // Кое-где пишут, что есть еще употребление "в виду гор" в значении "там, откуда видны горы".
     // Никогда не слышал, чтобы так говорили. Если в эту классификацию это вписывать,
     // я не уверен, EXPOSURE это, CONDITION или что-то третье.
-    addConfig(m, LocativeFormAttribute.EXPOSURE, v.concat(na), 'вид');
-    addConfig(m, LocativeFormAttribute.EXPOSURE, na, 'слух,счёт,ветер,ветр,свет');
-    addConfig(m, LocativeFormAttribute.MOTION, na, 'ход,бег,вес');
+    addConfig(masc, LocativeFormAttribute.EXPOSURE, v.concat(na), 'вид');
+    addConfig(masc, LocativeFormAttribute.EXPOSURE, na, 'слух,счёт,ветер,ветр,свет');
+    addConfig(masc, LocativeFormAttribute.MOTION, na, 'ход,бег,вес');
     // Пока непонятно, как разграничить "на каждом шагу" и "на первом шаге".
-    addConfig(m, LocativeFormAttribute.MOTION|LocativeFormAttribute.WITH_ADJECTIVE, na, 'шаг');
-    addConfig(m, LocativeFormAttribute.EVENT, na, 'бал,пир');
+    addConfig(masc, LocativeFormAttribute.MOTION|LocativeFormAttribute.WITH_ADJECTIVE, na, 'шаг');
+    addConfig(masc, LocativeFormAttribute.EVENT, na, 'бал,пир');
     // Может быть "дух" когда-то и значило "исповедь",
     // сейчас это только всех запутает.
-    addConfig(m, LocativeFormAttribute.CONDITION, na, 'дух,плав');
+    addConfig(masc, LocativeFormAttribute.CONDITION, na, 'дух,плав');
     // На полном газу. Не уверен, как это сюда записать. Вроде, устойчивое выражение.
-    addConfig(m, LocativeFormAttribute.MOTION|LocativeFormAttribute.WITH_ADJECTIVE, na, 'газ');
+    addConfig(masc, LocativeFormAttribute.MOTION|LocativeFormAttribute.WITH_ADJECTIVE, na, 'газ');
 
     // 1 и 5.
-    addConfig(m, LocativeFormAttribute.CONTAINER, v, 'глаз,зоб,нос,шкаф');
-    addConfig(m, LocativeFormAttribute.CONTAINER, vo, 'лоб');
-    addConfig(m, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, 'глаз,лоб,нос,шкаф,холм');
+    addConfig(masc, LocativeFormAttribute.CONTAINER, v, 'глаз,зоб,нос,шкаф');
+    addConfig(masc, LocativeFormAttribute.CONTAINER, vo, 'лоб');
+    addConfig(masc, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, 'глаз,лоб,нос,шкаф,холм');
 
     let two_and_five = 'бок,верх,зад,угол';
-    addConfig(m, LocativeFormAttribute.LOCATION, v, two_and_five);
-    addConfig(m, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, two_and_five);
+    addConfig(masc, LocativeFormAttribute.LOCATION, v, two_and_five);
+    addConfig(masc, LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE, na, two_and_five);
     // Есть сомнения, в каких случаях используется форма предложного падежа.
     // Является ли решающим наличие любого определения (в *Красноярском* крае, на *внешнем* крае)
     // или подобные выражения являются исключениями и их нельзя обобщать.
     // Я пока что склоняюсь к первому варианту.
-    addConfig(m, LocativeFormAttribute.LOCATION|LocativeFormAttribute.WITHOUT_ADJECTIVE, v, 'край');
-    addConfig(m,
+    addConfig(masc, LocativeFormAttribute.LOCATION|LocativeFormAttribute.WITHOUT_ADJECTIVE, v, 'край');
+    addConfig(masc,
         LocativeFormAttribute.OBJECT_WITH_FUNCTIONAL_SURFACE|LocativeFormAttribute.WITHOUT_ADJECTIVE,
         na, 'край');
 
     // 4 и 6
-    addConfig(m, LocativeFormAttribute.SURFACE, na, 'лёд,мох,снег');
-    addConfig(m, LocativeFormAttribute.SUBSTANCE, vo, 'лёд,лён,мох');
-    addConfig(m, LocativeFormAttribute.SUBSTANCE, v, 'снег');
+    addConfig(masc, LocativeFormAttribute.SURFACE, na, 'лёд,мох,снег');
+    addConfig(masc, LocativeFormAttribute.SUBSTANCE, vo, 'лёд,лён,мох');
+    addConfig(masc, LocativeFormAttribute.SUBSTANCE, v, 'снег');
 
     // А также, у слов женского рода третьего склонения с особыми формами
     // локатива пять семантических классов.
